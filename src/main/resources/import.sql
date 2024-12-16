3ac8aa5bb4fbc6b15ffbf4e1846d39fd69cf2a5e
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-12-16 19:19:42 -0300

    SEED da base de dados

---
+++ src/main/resources/import.sql
@@ -0,0 +1,18 @@
+INSERT INTO tb_client ( cpf, name, income, birth_date, children ) VALUES ( '000.000.000-00', 'Harry Potter', 7500.00,'1980-07-31', 3 );
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('111.111.111-11', 'Hermione Granger', 8500.00, '1979-09-19', 2);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('222.222.222-22', 'Ron Weasley', 6000.00, '1980-03-01', 3);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('333.333.333-33', 'Ginny Weasley', 7000.00, '1981-08-11', 3);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('444.444.444-44', 'Draco Malfoy', 9000.00, '1980-06-05', 1);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('555.555.555-55', 'Luna Lovegood', 6500.00, '1981-02-13', 1);
+
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('666.666.666-66', 'Vi', 5000.00, '2000-07-27', 0);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('777.777.777-77', 'Jinx', 4800.00, '2002-04-04', 0);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('888.888.888-88', 'Caitlyn', 7000.00, '1999-09-12', 0);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('999.999.999-99', 'Jayce', 8000.00, '1997-11-15', 0);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('111.222.333-44', 'Viktor', 6500.00, '1998-03-27', 0);
+
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('222.333.444-55', 'Tony Soprano', 15000.00, '1959-08-22', 2);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('333.444.555-66', 'Carmela Soprano', 12000.00, '1960-11-18', 2);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('444.555.666-77', 'Christopher Moltisanti', 8000.00, '1969-07-20', 1);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('555.666.777-88', 'Paulie Gualtieri', 7500.00, '1944-04-12', 0);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('666.777.888-99', 'Silvio Dante', 10000.00, '1957-11-21', 1);
