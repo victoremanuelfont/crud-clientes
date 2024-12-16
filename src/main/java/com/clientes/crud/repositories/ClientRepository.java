79e4181a75b2b9e0d436d40603da74cccaf1a11d
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-12-16 20:01:37 -0300

    Preechendo o ClientRepository - JPA

--- src/main/java/com/clientes/crud/repositories/ClientRepository.java
+++ src/main/java/com/clientes/crud/repositories/ClientRepository.java
@@ -1,4 +1,8 @@
 package com.clientes.crud.repositories;
 
-public interface ClientRepository {
+
+import com.clientes.crud.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long>{
 }
--- src/main/java/com/clientes/crud/service/ClientService.java
+++ src/main/java/com/clientes/crud/service/ClientService.java
@@ -1,4 +1,11 @@
 package com.clientes.crud.service;
 
+import com.clientes.crud.dto.ClientDTO;
+import com.clientes.crud.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class ClientService {
+
 }
