b5b4ecab43f3f94b361ae741ca79e6d134521bc9
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-12-16 18:44:49 -0300

    Anotation para relacionar a classe ao banco H2

--- src/main/java/com/clientes/crud/entities/Client.java
+++ src/main/java/com/clientes/crud/entities/Client.java
@@ -1,9 +1,15 @@
 package com.clientes.crud.entities;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "tb_client" )
 public class Client {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String cpf;
