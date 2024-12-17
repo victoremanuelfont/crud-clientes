185f3dc55a2fc6eca3c722be38e0972370d0b5a1
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-12-17 16:43:19 -0300

    Inserindo e construtor vazio

--- src/main/java/com/clientes/crud/service/ClientService.java
+++ src/main/java/com/clientes/crud/service/ClientService.java
@@ -37,5 +37,22 @@ public class ClientService {
     }
 
 
+    @Transactional
+    public ClientDTO insert (ClientDTO dto){
+        Client entity = new Client();
+        copyDtoToEntity(dto,entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+
+    private void copyDtoToEntity(ClientDTO dto, Client entity){
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+    }
+
 
 }
