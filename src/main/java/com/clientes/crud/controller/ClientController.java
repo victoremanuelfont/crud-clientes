79ccb4d6ec573fd13bab72f5ad2812c17aaa4b78
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-12-17 14:15:03 -0300

    Response Entity e Error específico 404

--- src/main/java/com/clientes/crud/controller/ClientController.java
+++ src/main/java/com/clientes/crud/controller/ClientController.java
@@ -5,6 +5,7 @@ import com.clientes.crud.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,9 +23,9 @@ public class ClientController {
 
 
     @GetMapping(value = "/{id}")
-    public ClientDTO findById(@PathVariable Long id){
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
         ClientDTO dto = service.findById(id);
-        return dto;
+        return ResponseEntity.ok(dto);
     }
 
     @GetMapping
