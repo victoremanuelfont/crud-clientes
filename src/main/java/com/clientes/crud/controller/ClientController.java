2e7f1b9c70382b39b8cd8bb83be2c34ab230229b
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-12-17 09:16:15 -0300

    Busca por todos os clientes mas paginados

--- src/main/java/com/clientes/crud/controller/ClientController.java
+++ src/main/java/com/clientes/crud/controller/ClientController.java
@@ -3,6 +3,8 @@ package com.clientes.crud.controller;
 import com.clientes.crud.dto.ClientDTO;
 import com.clientes.crud.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +28,11 @@ public class ClientController {
     }
 
     @GetMapping
-    public List<ClientDTO> findAll(){
-        return service.findAll();
+    public Page<ClientDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
     }
+
+
 
 
 
--- src/main/java/com/clientes/crud/service/ClientService.java
+++ src/main/java/com/clientes/crud/service/ClientService.java
@@ -4,6 +4,8 @@ import com.clientes.crud.dto.ClientDTO;
 import com.clientes.crud.entities.Client;
 import com.clientes.crud.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,9 +28,9 @@ public class ClientService {
 
 
     @Transactional(readOnly = true)
-    public List<ClientDTO> findAll(){
-        List<Client> result = repository.findAll();
-        return result.stream().map(x-> new ClientDTO(x)).toList();
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x-> new ClientDTO(x));
     }
 
 
